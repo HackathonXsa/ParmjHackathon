3acb8c353a2c68a797bcfd08f1a30009bba7260b
sasmaq <37103211+sasmaq@users.noreply.github.com>
2022-03-10 17:29:31 +0300

    select hackathon for email notifications

--- app/Http/Controllers/emailNotificationController.php
+++ app/Http/Controllers/emailNotificationController.php
@@ -5,27 +5,33 @@ namespace App\Http\Controllers;
 use App\Models\HackathonUsers;
 use App\Notifications\emailNotification;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Notification;
 use Illuminate\Support\Facades\Session;
 
 class emailNotificationController extends Controller
 {
     public function sendEmailNotificationView(){
-        return view('sendEmails');
+        $hackathon_users = DB::table('hackathon_users')->select('hackathon_id', 'hackathon_name')->distinct()->get();
+        return view('sendEmails')->with('hackathon_users', $hackathon_users);
     }
     public function sendEmailNotification(){
-        
-        $users = HackathonUsers::all('email');
+        $hackathon_users = DB::table('hackathon_users')->select('hackathon_id', 'hackathon_name')->distinct()->get();
 
-        
         $inputs = request()->validate([
+            'hackathon_select'=>'required',
             'subject'=>'required',
             'body'=>'required'
         ]);
+        if ($inputs['hackathon_select'] == 'all'){
+            $users = HackathonUsers::all('email');
+        } else {
+            $users = HackathonUsers::where('hackathon_id', $inputs['hackathon_select'])->get('email');;
+        };
         
         Notification::send($users, new emailNotification($inputs));
         
         session()->flash('post-created-message', 'Email Notifications was sent');
-        return view('sendEmails');
+        return view('sendEmails')->with('hackathon_users', $hackathon_users);
     }
 }
